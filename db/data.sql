7c88d0b894f57b5f362862f93becc4e220ffdf92
Soma Ueda <sora@turenar.xyz>
2017-06-18 01:05:00 +0900

    mock sql: update geom_hash

--- db/data.sql
+++ db/data.sql
@@ -8,6 +8,6 @@ VALUES (1, 'hoge@example.com', '$2y$10$ulqpqhnysOSiV1CiFlFMXeSC2vl4YzT7wNUtUAYmP
 
 INSERT INTO shop (name, open_time, close_time, address, latitude, longitude, geom_hash)
 VALUES
-	('おいしいケーキ デ・リ・シャス', '10:00:00', '20:00:00', '東京都百代田区百代田', 35.697372, 139.750131, '13300211231020221');
+	('おいしいケーキ デ・リ・シャス', '10:00:00', '20:00:00', '東京都百代田区百代田1-1-1', 35.697372, 139.750131, '133002112310202210');
 
 COMMIT;
