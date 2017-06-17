d110cd280632995cafc2c6dbddea4b5223d9b492
Soma Ueda <sora@turenar.xyz>
2017-06-18 01:43:12 +0900

    /review/search

--- src/routes/review.php
+++ src/routes/review.php
@@ -39,3 +39,23 @@ $app->post('/review/create', function (ServerRequestInterface $request, Response
 		->save();
 	return get_renderer()->render($response, ['review' => $review->render()]);
 })->add(new AuthMiddleware());
+
+$app->get('/review/search', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+	$data = $request->getQueryParams();
+	$lat_min = $data['lat_min'];
+	$lat_max = $data['lat_max'];
+	$long_min = $data['long_min'];
+	$long_max = $data['long_max'];
+
+	$quad_key = new QuadKey();
+	$hash_min = $quad_key->latLngToQuadKey($lat_min, $long_min, QUAD_KEY_LEVEL);
+	$hash_max = $quad_key->latLngToQuadKey($lat_max, $long_max, QUAD_KEY_LEVEL);
+//	return get_renderer()->renderAsError($response, 403, 'hoge', 'fuga', [$hash_min, $hash_max]);
+	$reviews = \ORM\ReviewQuery::create()
+		->filterByGeomHash(['min' => $hash_min, 'max' => $hash_max])
+		->filterByLatitude(['min' => $lat_min, 'max' => $lat_max])
+		->filterByLongitude(['min' => $long_min, 'max' => $long_max])
+		->find();
+
+	return get_renderer()->render($response, ['review' => render_as_json($reviews)]);
+});
