ba9290a2bc428f8945c69a60bb7d5113e16c9856
Soma Ueda <sora@turenar.xyz>
2017-06-17 17:51:34 +0900

    validation

--- src/middleware.php
+++ src/middleware.php
@@ -1,7 +1,5 @@
 <?php
 // Application middleware
-require_once __DIR__ . '/middleware/RequestValidateMiddleware.php';
-require_once __DIR__ . '/middleware/AuthMiddleware.php';
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
-// $app->add(new RequestValidateMiddleware());
+$app->add(new \Middleware\ValidationMiddleware());
--- src/settings.php
+++ src/settings.php
@@ -1,17 +1,17 @@
 <?php
 return [
-    'settings' => [
-        'displayErrorDetails' => false, // set to false in production
-        'addContentLengthHeader' => true, // Allow the web server to send the content-length header
+	'settings' => [
+		'displayErrorDetails' => false, // set to false in production
+		'addContentLengthHeader' => true, // Allow the web server to send the content-length header
+		'determineRouteBeforeAppMiddleware' => true,
+		// Renderer settings
+		'renderer' => [],
 
-        // Renderer settings
-	    'renderer' => [],
-
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
-    ],
+		// Monolog settings
+		'logger' => [
+			'name' => 'slim-app',
+			'path' => __DIR__ . '/../logs/app.log',
+			'level' => \Monolog\Logger::DEBUG,
+		],
+	],
 ];
