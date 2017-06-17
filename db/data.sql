929ef921f895aacb51caa054ba9fd51b5b15a72a
Soma Ueda <sora@turenar.xyz>
2017-06-18 00:30:21 +0900

    mock user

--- db/data.sql
+++ db/data.sql
@@ -1,5 +1,11 @@
 BEGIN;
 
+INSERT INTO user (user_id)
+VALUES (1);
+
+INSERT INTO authentication (user_id, email, password, token)
+VALUES (1, 'hoge@example.com', '$2y$10$ulqpqhnysOSiV1CiFlFMXeSC2vl4YzT7wNUtUAYmPF/GqGxuZSyAm', 'token-string');
+
 INSERT INTO shop (name, open_time, close_time, address, latitude, longitude, geom_hash)
 VALUES
 	('おいしいケーキ デ・リ・シャス', '10:00:00', '20:00:00', '東京都百代田区百代田', 35.697372, 139.750131, '13300211231020221');
