5b23ab2951972b3801eec59710db7a63dae6eb37
Soma Ueda <sora@turenar.xyz>
2017-06-17 18:39:05 +0900

    add shop_id for shop

--- src/routes/shop.php
+++ src/routes/shop.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 $app->get('/shop/{id:\d+}', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
 	return get_renderer()->render($response, [
 		'shop' => [
+			'shop_id' => 1,
 			'name' => 'おいしいケーキ デ・リ・シャス',
 			'address' => '東京都百代田区百代田1-1-1',
 			'latitude' => 139.691,
@@ -20,6 +21,7 @@ $app->get('/shop/search', function (ServerRequestInterface $request, ResponseInt
 	return get_renderer()->render($response, [
 		'shop' => [
 			[
+				'shop_id' => 1,
 				'name' => 'おいしいケーキ デ・リ・シャス',
 				'address' => '東京都百代田区百代田1-1-1',
 				'latitude' => 139.691,
