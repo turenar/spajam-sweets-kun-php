d2b4785db762ab55576d72a2e2de0f3ea42a6fb6
Soma Ueda <sora@turenar.xyz>
2017-06-18 00:19:32 +0900

    add review table

---
+++ db/generated-classes/ORM/Review.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ORM;
+
+use ORM\Base\Review as BaseReview;
+
+/**
+ * Skeleton subclass for representing a row from the 'review' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Review extends BaseReview
+{
+
+	public function render()
+	{
+		return [
+			'review_id' => $this->getReviewId(),
+			'shop_id' => $this->getShopId(),
+			'user_id' => $this->getUserId(),
+			'rating' => $this->getRating(),
+			'review_text' => $this->getReviewText(),
+			'sweet_type' => $this->getSweetType(),
+			'latitude'=>$this->getLatitude(),
+			'longitude' => $this->getLongitude(),
+		];
+	}
+}
---
+++ src/routes/review.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sora
+ * Date: 6/17/17
+ * Time: 10:49 PM
+ */
