565e63bc623b89f7c91f0f7f590e3649711e377a
Soma Ueda <sora@turenar.xyz>
2017-06-18 03:18:00 +0900

    DELETE /review/:id/likes

--- src/routes/review/likes.php
+++ src/routes/review/likes.php
@@ -44,3 +44,39 @@ $app->post('/review/{id:\d+}/likes', function (ServerRequestInterface $request, 
 	}
 	return get_renderer()->renderAsError($response, 503, 'Transaction Failure', 'しばらく待ってからもう一度お試しください。');
 })->add(new AuthMiddleware())->setArgument('validator.basePath', 'review/likes');
+
+$app->delete('/review/{id:\d+}/likes', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+	$id = $args['id'];
+	/** @var \ORM\User $user */
+	$user = $request->getAttribute('user');
+
+	for ($i = 0; $i < TRANSACTION_PATIENCE; $i++) {
+		$result = transaction(function () use ($response, $user, $id) {
+			$review = \ORM\ReviewQuery::create()
+				->filterByReviewId($id)
+				->findOne();
+			if ($review === null) {
+				return get_renderer()->renderAsError($response, 404, 'Not Found', '指定したレビューが見つかりません');
+			}
+
+			$deleted = \ORM\LikesQuery::create()
+				->filterByUserId($user->getUserId())
+				->filterByReviewId($review->getReviewId())
+				->delete();
+			if ($deleted) {
+				$atomic_rewritten = \ORM\ReviewQuery::create()
+					->filterByReviewId($review->getReviewId())
+					->filterByLike($review->getLike())
+					->update(['Like' => $review->getLike() - 1]);
+				if (!$atomic_rewritten) {
+					return null;
+				}
+			}
+			return get_renderer()->render($response, []);
+		});
+		if ($result !== null) {
+			return $result;
+		}
+	}
+	return get_renderer()->renderAsError($response, 503, 'Transaction Failure', 'しばらく待ってからもう一度お試しください。');
+})->add(new AuthMiddleware())->setArgument('validator.basePath', 'review/likes');
