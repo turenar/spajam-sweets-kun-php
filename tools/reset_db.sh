85f7db830fef8d52203e1fa11ad57d4f445c1361
Soma Ueda <sora@turenar.xyz>
2017-06-18 01:05:49 +0900

    fix re-running resetdb.sh fails

--- tools/reset_db.sh
+++ tools/reset_db.sh
@@ -4,5 +4,7 @@ cd "$(dirname "$0")/.."
 source tools/common.sh
 
 exec_cmd tools/migrate_db.sh
+exec_cmd vendor/bin/propel sql:build
+exec_cmd vendor/bin/propel sql:insert
 exec_cmd php batch/exec_sql.php < db/data.sql
 print_message 'done.'
