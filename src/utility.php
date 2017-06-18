9fbfad47db50f7115a0766ee052572dcd4d42fe4
Soma Ueda <sora@turenar.xyz>
2017-06-18 10:06:56 +0900

    update sql

--- src/utility.php
+++ src/utility.php
@@ -4,7 +4,7 @@ use Propel\Runtime\ActiveRecord\ActiveRecordInterface;
 use Propel\Runtime\Collection\ObjectCollection;
 
 const QUAD_KEY_LEVEL = 18;
-const SPAWN_RANDOMIZE_RANGE = 300;
+const SPAWN_RANDOMIZE_RANGE = 1000;
 const RANDOM_ROUND = 3;
 const TRANSACTION_PATIENCE = 10;
 
